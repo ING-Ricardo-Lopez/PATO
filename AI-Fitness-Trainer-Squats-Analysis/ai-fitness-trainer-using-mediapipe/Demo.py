b2eb5febe39a951d3edcd27cb4260522e6e67691
HECTOR ULISES MENDIOLA MORALES <110561031+Ulises-Mendiola@users.noreply.github.com>
2023-11-08 12:38:10 -0700

    Update Demo.py

--- AI-Fitness-Trainer-Squats-Analysis/ai-fitness-trainer-using-mediapipe/Demo.py
+++ AI-Fitness-Trainer-Squats-Analysis/ai-fitness-trainer-using-mediapipe/Demo.py
@@ -7,12 +7,3 @@ st.title('AI PATO Trainer: Analisis de sentadilla')
 recorded_file = 'output_sample.mp4'
 sample_vid = st.empty()
 sample_vid.video(recorded_file)
-
-#jhiubuin
-
-    
-    
-
-    
-
-
