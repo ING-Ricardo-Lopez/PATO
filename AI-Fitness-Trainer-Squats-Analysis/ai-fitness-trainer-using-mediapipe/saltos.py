7f54ae0ebd063cdfc69181899745da8f9014cad4
ElRicaPija <rl0051844@gmail.com>
2023-11-20 23:49:04 -0700

    s

--- AI-Fitness-Trainer-Squats-Analysis/ai-fitness-trainer-using-mediapipe/saltos.py
+++ AI-Fitness-Trainer-Squats-Analysis/ai-fitness-trainer-using-mediapipe/saltos.py
@@ -8,7 +8,7 @@ contador = 0
 piso = 2848
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-cap = cv2.VideoCapture('output_sample.mp4')#no detecta la camara esta mamada
+cap = cv2.VideoCapture('output_sample.mp4')#no detecta la camara esta mamadaa
 
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while True:
